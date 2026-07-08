7346c3d7f780eb63b2ea35f9c32850f84d899aa7
Lingmo <lingmo@lingmo.org>
2026-07-08 16:42:30 +0800

    Fix LingmoPCH.cmake: add missing PUBLIC/PRIVATE keywords
    target_precompile_headers requires keyword arguments (PUBLIC, PRIVATE)
    before the header file lists. The cmake_parse_arguments macro strips
    them, so they must be re-added in the call.

--- cmake/LingmoPCH.cmake
+++ cmake/LingmoPCH.cmake
@@ -2,8 +2,8 @@ macro(lingmo_pch TARGET)
     cmake_parse_arguments(PCH "" "" "PUBLIC;PRIVATE" ${ARGN})
     if(PCH_PUBLIC OR PCH_PRIVATE)
         target_precompile_headers(${TARGET}
-            ${PCH_PUBLIC}
-            ${PCH_PRIVATE}
+            PUBLIC ${PCH_PUBLIC}
+            PRIVATE ${PCH_PRIVATE}
         )
     endif()
 endmacro()
